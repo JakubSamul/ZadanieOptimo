e87dbef3952f913598c890bd8c379e5e458cbb6a
JakubSamul <jakubsamul0@gmail.com>
2024-09-05 19:29:16 +0200

    add class Task

--- app.py
+++ app.py
@@ -3,6 +3,13 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return "Zadanie"
+class Task:
+    def __init__(self, id, title, description, status="do zrobienia"):
+        self.id = id
+        self.title = title
+        self.description = description
+        self.status = status
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
